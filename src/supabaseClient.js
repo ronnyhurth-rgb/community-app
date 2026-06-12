d8ba53eaa4eb6f28f769b83225b84941e4400d92
ronnyhurth-rgb <ronny.hurth@gmail.com>
2026-06-12 07:30:56 +0200

    redirect URL

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Wichtig: Vite verwendet import.meta.env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("Supabase-Schlüssel fehlen!");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+redirectTo: 'https://community-app1-34hzhimmf-ronnyhurth-7948s-projects.vercel.app/'
